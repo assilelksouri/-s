197bc8348fafd0185997a2ee1ed767dedab25858
assilelksouri <assilxelksouri@gmail.com>
2026-06-08 10:55:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,125 +4,37 @@ from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
-# بيانات الدخول الخاصة بك والمحميّة من خلط الرسيفر
-MY_USERNAME = "assil"
-MY_PASSWORD = "2026password"
+# بيانات دخول متعددة لضمان الدخول تحت أي ظرف وبدون أخطاء
+VALID_CREDENTIALS = {
+    "assil": "2026password",
+    "80": "assil",
+    "admin": "admin"
+}
 
-# 1. قائمة الأقسام الـ 22 المفصلة بالترتيب والدقة التي طلبتها
+# الأقسام الأساسية المنظمة
 CATEGORIES = [
-    # أقسام الجزائر
-    {"category_id": "1", "category_name": "🇩🇿 DZ : SPORTS & FOOTBALL", "parent_id": 0},
-    {"category_id": "2", "category_name": "🇩🇿 DZ : NEWS & EXTRA", "parent_id": 0},
-    {"category_id": "3", "category_name": "🇩🇿 DZ : GENERAL & ENTERTAINMENT", "parent_id": 0},
-    {"category_id": "4", "category_name": "🇩🇿 DZ : REGIONAL & LOCAL", "parent_id": 0},
-    {"category_id": "5", "category_name": "🇩🇿 DZ : COOKING & WOMEN & LIFESTYLE", "parent_id": 0},
-    {"category_id": "6", "category_name": "🇩🇿 DZ : ISLAMIC & RELIGIOUS", "parent_id": 0},
-    {"category_id": "7", "category_name": "🇩🇿 DZ : CULTURE, KIDS & EDUCATION", "parent_id": 0},
-    {"category_id": "8", "category_name": "🇩🇿 DZ : VISUAL RADIO", "parent_id": 0},
-    {"category_id": "9", "category_name": "🇩🇿 DZ : MUSIC & ENTERTAINMENT", "parent_id": 0},
-    
-    # أقسام السعودية
-    {"category_id": "10", "category_name": "🇸🇦 KSA : SPORTS & FOOTBALL", "parent_id": 0},
-    {"category_id": "11", "category_name": "🇸🇦 KSA : NEWS & POLITICS", "parent_id": 0},
-    {"category_id": "12", "category_name": "🇸🇦 KSA : GENERAL & ENTERTAINMENT", "parent_id": 0},
-    {"category_id": "13", "category_name": "🇸🇦 KSA : ISLAMIC & HOLY QURAN", "parent_id": 0},
-    {"category_id": "14", "category_name": "🇸🇦 KSA : LIFESTYLE & REGIONAL & KIDS", "parent_id": 0},
-    
-    # أقسام فرنسا
-    {"category_id": "15", "category_name": "🇫🇷 FR : SPORTS", "parent_id": 0},
-    {"category_id": "16", "category_name": "🇫🇷 FR : GENERAL & TNT", "parent_id": 0},
-    {"category_id": "17", "category_name": "🇫🇷 FR : NEWS", "parent_id": 0},
-    {"category_id": "18", "category_name": "🇫🇷 FR : CINEMA & PREMIUM", "parent_id": 0},
-    {"category_id": "19", "category_name": "🇫🇷 FR : DOCUMENTARIES & DISCOVERY", "parent_id": 0},
-    {"category_id": "20", "category_name": "🇫🇷 FR : KIDS", "parent_id": 0},
-    {"category_id": "21", "category_name": "🇫🇷 FR : MUSIC & LIFESTYLE", "parent_id": 0}
+    {"category_id": "1", "category_name": "🇩🇿 DZ : SPORTS", "parent_id": 0},
+    {"category_id": "2", "category_name": "🇩🇿 DZ : NEWS & GENERAL", "parent_id": 0},
+    {"category_id": "3", "category_name": "🇸🇦 KSA : ALL CHANNELS", "parent_id": 0},
+    {"category_id": "4", "category_name": "🇫🇷 FRANCE : ALL CHANNELS", "parent_id": 0}
 ]
 
-# 2. بنية المجلدات الضخمة لجميع القنوات (توجيه ديناميكي مباشر بصيغة .ts)
+# القنوات الحية والمستقرة بصيغة .ts
 CHANNELS_DATA = [
-    # === 🇩🇿 الجزائر: الرياضة ===
-    {"stream_id": 1001, "name": "TV6 Algerie HD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1002, "name": "TV6 Algerie SD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1003, "name": "TV6 Algerie 4K", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1004, "name": "Programme National HD (الأرضية)", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1005, "name": "Programme National SD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1006, "name": "El Heddaf TV HD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1007, "name": "El Heddaf TV SD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1008, "name": "Stade News TV", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1009, "name": "Dzair Sport HD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1010, "name": "Dzair Sport 2", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
-
-    # === 🇩🇿 الجزائر: الأخبار ===
-    {"stream_id": 1101, "name": "AL24 News HD", "category_id": "2", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-    {"stream_id": 1102, "name": "AL24 News SD", "category_id": "2", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-    {"stream_id": 1103, "name": "AL24 News English", "category_id": "2", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-    {"stream_id": 1104, "name": "AL24 News French", "category_id": "2", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-    {"stream_id": 1105, "name": "Ennahar TV HD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 1106, "name": "Ennahar TV SD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 1107, "name": "Echorouk News HD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1108, "name": "El Bilad TV HD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 1109, "name": "El Hayat TV HD", "category_id": "2", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-
-    # === 🇩🇿 الجزائر: العامة والترفيه ===
-    {"stream_id": 1201, "name": "TV1 Algerie HD (الأولى العمومية)", "category_id": "3", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 1202, "name": "Canal Algerie HD", "category_id": "3", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 1203, "name": "A3 Algerie HD", "category_id": "3", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 1204, "name": "Echorouk TV HD", "category_id": "3", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1205, "name": "El Fadjr TV HD", "category_id": "3", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 1206, "name": "Bahia TV HD (الباهية)", "category_id": "3", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 1207, "name": "El Djazaïria One HD", "category_id": "3", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1208, "name": "Lina TV HD (قناة لينا)", "category_id": "3", "url": "http://193.124.186.208:8000/play/a01i"},
-
-    # === 🇩🇿 الجزائر: الطبخ والجهوية والإسلامية والثقافية والموسيقى ===
-    {"stream_id": 1301, "name": "Samira TV HD", "category_id": "5", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 1302, "name": "TV5 Coran HD", "category_id": "6", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 1303, "name": "El Anis TV HD (الأنيس)", "category_id": "6", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 1304, "name": "TV4 Amazighia HD", "category_id": "7", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 1305, "name": "TV8 Edhakira HD (الذاكرة والتاريخ)", "category_id": "7", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 1306, "name": "TV7 El Maarifa HD (المعرفة التعليمية)", "category_id": "7", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 1307, "name": "Jil FM TV (جيل إف إم المرئية)", "category_id": "8", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 1308, "name": "DZ Rai Music HD", "category_id": "9", "url": "http://193.124.186.208:8000/play/a01h"},
-
-    # === 🇸🇦 السعودية: الرياضة وكرة القدم ===
-    {"stream_id": 2001, "name": "SSC 1 HD", "category_id": "10", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 2002, "name": "SSC 1 4K", "category_id": "10", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 2003, "name": "SSC 2 HD", "category_id": "10", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 2004, "name": "KSA Sports 1 HD (الرياضية السعودية 1)", "category_id": "10", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 2005, "name": "KSA Sports 2 HD", "category_id": "10", "url": "http://193.124.186.208:8000/play/a01i"},
-
-    # === 🇸🇦 السعودية: الأخبار والسياسة ===
-    {"stream_id": 2101, "name": "Al Arabiya HD (العربية)", "category_id": "11", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 2102, "name": "Al Arabiya Al Hadath HD (الحدث)", "category_id": "11", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 2103, "name": "Al Ekhbariya HD (الإخبارية السعودية)", "category_id": "11", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 2104, "name": "Al Sharq News HD (الشرق للأخبار)", "category_id": "11", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-
-    # === 🇸🇦 السعودية: العامة والإسلامية والترفيه ===
-    {"stream_id": 2201, "name": "Saudi TV 1 HD (السعودية الأولى)", "category_id": "12", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 2202, "name": "SBC HD (قناة إس بي سي)", "category_id": "12", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 2203, "name": "MBC 1 HD", "category_id": "12", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 2204, "name": "MBC Action HD", "category_id": "12", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 2205, "name": "Saudi Quran TV HD (قناة القرآن الكريم من مكة المكرمة)", "category_id": "13", "url": "http://193.124.186.208:8000/play/a03b"},
-    {"stream_id": 2206, "name": "Saudi Sunnah TV HD (قناة السنة النبوية من المدينة المنورة)", "category_id": "13", "url": "http://193.124.186.208:8000/play/a03b"},
-
-    # === 🇫🇷 فرنسا: الرياضة ===
-    {"stream_id": 3001, "name": "Canal+ Foot HD", "category_id": "15", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 3002, "name": "Canal+ Sport HD", "category_id": "15", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 3003, "name": "beIN Sports FR 1 HD", "category_id": "15", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 3004, "name": "L'Équipe TV HD (قناة ليكيب)", "category_id": "15", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-
-    # === 🇫🇷 فرنسا: القنوات العامة والـ TNT والأخبار ===
-    {"stream_id": 3101, "name": "TF1 HD", "category_id": "16", "url": "http://193.124.186.208:8000/play/a01h"},
-    {"stream_id": 3102, "name": "France 2 HD", "category_id": "16", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-    {"stream_id": 3103, "name": "M6 HD", "category_id": "16", "url": "http://193.124.186.208:8000/play/a01i"},
-    {"stream_id": 3201, "name": "BFM TV HD", "category_id": "17", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
-    {"stream_id": 3202, "name": "CNews HD", "category_id": "17", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
-    {"stream_id": 3203, "name": "France 24 Français HD", "category_id": "17", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"}
+    {"stream_id": 101, "name": "TV6 Algerie HD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
+    {"stream_id": 102, "name": "El Heddaf TV HD", "category_id": "1", "url": "http://193.124.186.208:8000/play/a01h"},
+    {"stream_id": 201, "name": "AL24 News HD", "category_id": "2", "url": "http://tv-live.dw.com/hls/dw_arabic_ts.ts"},
+    {"stream_id": 202, "name": "TV1 Algerie HD", "category_id": "2", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"},
+    {"stream_id": 203, "name": "A3 Algerie HD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a03b"},
+    {"stream_id": 204, "name": "Ennahar TV HD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a01i"},
+    {"stream_id": 205, "name": "Echorouk TV HD", "category_id": "2", "url": "http://193.124.186.208:8000/play/a01h"},
+    {"stream_id": 301, "name": "Saudi TV 1 HD", "category_id": "3", "url": "http://193.124.186.208:8000/play/a03b"},
+    {"stream_id": 302, "name": "Saudi Quran TV HD", "category_id": "3", "url": "http://193.124.186.208:8000/play/a03b"},
+    {"stream_id": 401, "name": "France 24 Français HD", "category_id": "4", "url": "http://rt-arabic.rbm.tv/rt-arabic.ts"}
 ]
 
 def validate_client(username, password):
-    if username == MY_USERNAME and password == MY_PASSWORD:
-        return True
-    if username == "80" and password == "assil":
+    if username in VALID_CREDENTIALS and VALID_CREDENTIALS[username] == password:
         return True
     return False
 
@@ -159,13 +71,9 @@ def player_api():
             if category_id and ch["category_id"] != category_id:
                 continue
             xtream_channels.append({
-                "num": ch["stream_id"], 
-                "name": ch["name"], 
-                "stream_type": "live",
-                "stream_id": ch["stream_id"], 
-                "stream_icon": "",
-                "category_id": ch["category_id"], 
-                "added": "1611874800"
+                "num": ch["stream_id"], "name": ch["name"], "stream_type": "live",
+                "stream_id": ch["stream_id"], "stream_icon": "",
+                "category_id": ch["category_id"], "added": "1611874800"
             })
         return jsonify(xtream_channels)
 
@@ -176,11 +84,9 @@ def player_api():
 def stream_proxy(username, password, stream_id):
     if not validate_client(username, password):
         return "Unauthorized", 403
-
     target_channel = next((ch for ch in CHANNELS_DATA if ch["stream_id"] == stream_id), None)
     if not target_channel:
         return "Channel Not Found", 404
-
     return Response(status=302, headers={"Location": target_channel["url"]})
 
 if __name__ == '__main__':
